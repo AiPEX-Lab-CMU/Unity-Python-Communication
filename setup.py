a5abaecdd452f37158132ab79f603ef4367cf014
pengyun961012 <pengyun@umich.edu>
2020-01-24 11:26:43 -0500

    added a setup script to install the required modules

---
+++ setup.py
@@ -0,0 +1,9 @@
+import subprocess
+import sys
+
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'zmq'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'numpy-stl'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'scipy'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'imageio'])
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'Pillow'])
